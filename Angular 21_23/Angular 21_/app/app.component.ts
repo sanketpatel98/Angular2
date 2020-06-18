03a92d4471b946a613be0e4dfae105dd8b299ea8
sanketpatel98 <48122030+sanketpatel98@users.noreply.github.com>
2020-06-18 17:24:59 +0530

    Angular 21_23 MDF Forms

---
+++ Angular 21_23/Angular 21_/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators, FormBuilder } from '@angular/forms';
+@Component({
+  selector: 'my-app',
+  templateUrl: `app/app.component.html`,
+  styles: [`
+     input.ng-invalid {border-left: 5px solid red;}
+     input.ng-valid {border-left: 5px solid green;}
+  `]
+})
+export class AppComponent implements OnInit { 
+  /*
+  userForm = new FormGroup({
+    name: new FormControl("Sanket",[Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
+    email: new FormControl(),
+    address: new FormGroup({
+      street: new FormControl(),
+      city: new FormControl(),
+      postalcode: new FormControl(null, Validators.pattern("^[1-9][0-9]{4}$"))  
+    })
+  });*/
+  userForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder){}
+
+  ngOnInit()
+  {
+    this.userForm = this._formBuilder.group({
+      name: ['Sanket', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+      email: [],
+      address: this._formBuilder.group({
+        street:[],
+        city:[],
+        postalcode:[null,[Validators.pattern("^[1-9][0-9]{4}$")]]
+      })
+
+    })
+  }
+  onSubmit()
+  {
+    console.log(this.userForm.value);
+  }
+}
